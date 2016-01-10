6592f770ca0b1b8474ed7e902cf4a1cf608bd8a7
fuzongjian <fdkj0002@fdkjCSRdeMac-mini.local>
2016-01-10 13:22:06 +0800

    1.0
    创建空的工程

---
+++ FZJPhotosFrameWork/FZJPhotosFrameWork/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FZJPhotosFrameWork
+//
+//  Created by fdkj0002 on 16/1/10.
+//  Copyright © 2016年 fdkj0002. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FZJPhotosFrameWork/FZJPhotosFrameWork/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FZJPhotosFrameWork
+//
+//  Created by fdkj0002 on 16/1/10.
+//  Copyright © 2016年 fdkj0002. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
